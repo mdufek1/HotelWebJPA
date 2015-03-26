9d6214a1634624518f768c106ee3d9a00709bf80
Mike <Mike@10.30.18.75>
2015-03-26 11:44:15 -0700

    Began adding AJAX

--- src/main/java/hotel/web/controller/HotelDataControllerNonAJAX.java
+++ src/main/java/hotel/web/controller/HotelDataControllerNonAJAX.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
